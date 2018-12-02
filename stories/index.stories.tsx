4d50a757234773e648509cf70f3075a4b192baa3
zakariya <zakariya.harti@gmail.com>
2018-12-02 13:13:34 +0100

    update st 1

--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -78,6 +78,85 @@ stories.add('first example',() => {
     propTables: false,
     text: `
       ## Example code
+
+
+           import NotificationSystem from 'react-awesome-notifications';
+           import {connect} from 'react-redux';
+
+           class MyComponent extends React.Component>{
+
+           state = {
+            nots: [
+              {
+                level: 'primary',
+                dismissAfter: 0,
+                dismissible: true,
+                title: 'a new react.js version has been released',
+                message: 'a new react.js version has been released',
+                position: 'tr',
+                id: 'mlk',
+                allowHTML: true,
+                closeButton: true,
+                buttons: [
+                 {
+                   label: 'submit'
+                 }
+               ]
+             },
+             {
+               level: 'info',
+               dismissAfter: 0,
+               dismissible: true,
+               title: 'it\'s been a while no see',
+                message: 'a new react.js version has been released',
+               position: 'bl',
+               id: 'mlkfg',
+               allowHTML: true,
+               closeButton: true,
+               buttons: [
+                {
+                  label: 'show'
+                }
+              ]
+            },
+            {
+              level: 'success',
+              dismissAfter: 0,
+              dismissible: true,
+              title: 'it\'s been a while no see',
+              message: 'a new react.js version has been released',
+              position: 'bl',
+              id: 'mlkfghj',
+              allowHTML: true,
+              closeButton: true,
+              buttons: [
+                {
+                  label: 'show'
+                }
+              ]
+             }
+           ]
+           };
+
+           addNotification = () => {
+              this.state.nots.map(not => {
+                this.props.addNotification(not);
+              });
+            }
+
+           render(){
+             return(
+               <div>
+                 <NotificationSystem />
+                 <div>
+                     <button onClick={this.addNotification}>click me!</button>
+                 </div>
+               </div>
+             );
+           }
+          }
+          export default connect(undefined, {addNotification})(PlayGround);
+
     `
   }
 })
